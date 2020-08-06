1b7fb7ea9825d548f4faacc6381b11346f29e319
Filip Tronicek <filip.tronicek@seznam.cz>
2020-08-06 02:18:32 +0200

    Add about section

--- js/components.js
+++ js/components.js
@@ -10,7 +10,12 @@ class Home extends HTMLElement {
 class About extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            About site lmao
+            Jedno z nejčastějších témat poslední doby je ochrana životního prostředí a s tím spojené sbírání a třídění odpadů. 
+            Aplikace GreenPoint je snahou o motivaci třídit opad pomocí hry, kdy hráč za každý vyhozený kus odpadu získává odměnu ve formě zelených lístků. 
+            Díky lístkům se dají poté dokupovat různá vylepšení aplikace (jiný vzhled, … atd.). 
+            Lístky zároveň slouží jako jakési měření úrovně, kdy hráč s určitým počtem lístků se může účastnit soutěží o hodnotné ceny od našich sponzorů. 
+            V rámci aplikace je také možné hráči organizovat společné akce na sbírání odpadků. 
+            Za každou takovou akci se samozřejmě připočítávají lístky, a to v závislosti na počtu zúčastněných.
         `;
     }
 }

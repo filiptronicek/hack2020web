f4941e13527b4f3c951b18f89f34951cef2ab6e9
Filip Tronicek <filip.tronicek@seznam.cz>
2020-08-06 02:38:53 +0200

    Add all initial cites

---
+++ js/cites.js
@@ -0,0 +1,9 @@
+export default [
+    { name: "Ernest Hemingway", "cite": "The Earth is fine place and worth fighting for."},
+    { name: "Barbara Ward", cite: "We have forgotten how to be good guests, how to walk lightly on the earth as its other creatures do."},
+    { name: "Mother Teresa", cite: "I only feel angry when I see waste. When I see people throwing away things we could use."},
+    { name: "Wendell Berry", cite: "The Earth is what we all have in common."},
+    { name: "Margaret Mead", cite: "Never doubt that a small group of thoughtful, committed citizens can change the world; indeed, it is the only thing that ever has."},
+    { name: "Lady Bird Johnson", cite: "The environment is where we all meet; where we all have a mutual interest; it is the one thing all of us share."},
+    { name: "Wendell Berry", cite: "If you are fearful of the destruction of the environment, then learn to quit being an environmental parasite."}
+];
